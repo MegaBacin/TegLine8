4dec3e4a389474deaaf9738263f85cfcec6fcf71
CuriosityExp <dsianturi57@gmail.com>
2023-05-11 21:43:32 +0700

    add Event Listener

--- menu.js
+++ menu.js
@@ -1,24 +1,70 @@
 let items = [
-  { name: "Ayam Balado", price: 10000, stock: 5, type: "lauk" },
-  { name: "Ayam Goreng", price: 11000, stock: 5, type: "lauk" },
-  { name: "Ikan Goreng", price: 10000, stock: 4, type: "lauk" },
-  { name: "Ikan Sambel Ijo", price: 12000, stock: 3, type: "lauk" },
-  { name: "Cumi Tumis", price: 13000, stock: 2, type: "lauk" },
-  { name: "Cumi Oseng Asin", price: 14000, stock: 1, type: "lauk" },
-  { name: "Sayur Sop", price: 10000, stock: 5, type: "sayur" },
-  { name: "Sayur Asem", price: 11000, stock: 5, type: "sayur" },
-  { name: "Kangkung", price: 10000, stock: 4, type: "sayur" },
-  { name: "Cap Cay", price: 12000, stock: 3, type: "sayur" },
-  { name: "Teh Manis Hangat", price: 10000, stock: 5, type: "minuman" },
-  { name: "Teh Manis Dingin", price: 11000, stock: 5, type: "minuman" },
-  { name: "Teh Tawar Hangat", price: 10000, stock: 4, type: "minuman" },
-  { name: "Teh Tawar Dingin", price: 12000, stock: 3, type: "minuman" },
-  { name: "Air Mineral", price: 12000, stock: 3, type: "minuman" },
-  { name: "Jeruk Panas", price: 12000, stock: 3, type: "minuman" },
-  { name: "Jeruk Dingin", price: 12000, stock: 3, type: "minuman" },
-  { name: "Nasi Putih", price: 5000, stock: 3, type: "nasi" },
-  { name: "Nasi Uduk", price: 6000, stock: 3, type: "nasi" },
+  { id: 1, name: "Ayam Balado", price: 10000, stock: 5, type: "lauk" },
+  { id: 2, name: "Ayam Goreng", price: 11000, stock: 5, type: "lauk" },
+  { id: 3, name: "Ikan Goreng", price: 10000, stock: 4, type: "lauk" },
+  { id: 4, name: "Ikan Sambel Ijo", price: 12000, stock: 3, type: "lauk" },
+  { id: 5, name: "Cumi Tumis", price: 13000, stock: 2, type: "lauk" },
+  { id: 6, name: "Cumi Oseng Asin", price: 14000, stock: 1, type: "lauk" },
+  { id: 7, name: "Sayur Sop", price: 10000, stock: 5, type: "sayur" },
+  { id: 8, name: "Sayur Asem", price: 11000, stock: 5, type: "sayur" },
+  { id: 9, name: "Kangkung", price: 10000, stock: 4, type: "sayur" },
+  { id: 10, name: "Cap Cay", price: 12000, stock: 3, type: "sayur" },
+  { id: 11, name: "Teh Manis Hangat", price: 10000, stock: 5, type: "minuman" },
+  { id: 12, name: "Teh Manis Dingin", price: 11000, stock: 5, type: "minuman" },
+  { id: 13, name: "Teh Tawar Hangat", price: 10000, stock: 4, type: "minuman" },
+  { id: 14, name: "Teh Tawar Dingin", price: 12000, stock: 3, type: "minuman" },
+  { id: 15, name: "Air Mineral", price: 12000, stock: 3, type: "minuman" },
+  { id: 16, name: "Jeruk Panas", price: 12000, stock: 3, type: "minuman" },
+  { id: 17, name: "Jeruk Dingin", price: 12000, stock: 3, type: "minuman" },
+  { id: 18, name: "Nasi Putih", price: 5000, stock: 3, type: "nasi" },
+  { id: 19, name: "Nasi Uduk", price: 6000, stock: 3, type: "nasi" },
 ];
+
+function renderFirstTime(items){
+    let newCards = document.createElement("div");
+    newCards.id = 'cards';
+    let body = document.getElementsByTagName("body");
+    body[0].appendChild(newCards);
+    for(let perMakanan of items){
+        const card = document.createElement("div");
+        card.classList.add(`card`);
+        card.classList.add(perMakanan.type);
+        card.id = perMakanan.id;
+        const img = document.createElement("img");
+        img.classList.add("item-img");
+        img.src = `./assets/img/${perMakanan.name
+          .toLowerCase()
+          .replace(" ", "-")}.jpeg`;
+        img.alt = `${perMakanan.name}`;
+        const title = document.createElement("p");
+        title.innerText = `${perMakanan.name}`;
+        const harga = document.createElement("p");
+        const status = document.createElement("p");
+        title.classList.add(`item-title`);
+        harga.classList.add(`item-harga`);
+        harga.innerText = `${new Intl.NumberFormat("id-ID", {
+          style: "currency",
+          currency: "IDR",
+        }).format(perMakanan.price)}`;
+        status.classList.add(`item-status`);
+        if (perMakanan.stock !== 0) {
+          status.innerText = `Tersedia: ${perMakanan.stock}`;
+        } else {
+          status.innerText = `Habis`;
+          status.style = "color: red";
+        }
+        newCards.appendChild(card);
+        card.appendChild(title);
+        card.appendChild(harga);
+        card.appendChild(status);        
+    }
+    let cards = document.getElementsByClassName(`card`);
+    for (let card of cards) {
+      card.setAttribute("onclick", `addItemToCart(items,${card.id})`);
+    }
+}
+
+
 
 function render(filter,type){
     let cards = document.getElementById("cards");
@@ -26,13 +72,14 @@ function render(filter,type){
     let newCards = document.createElement('div');
     let body = document.getElementsByTagName('body');
     newCards.id =`cards`;
-    console.log(body);
+    // console.log(body);
     body[0].appendChild(newCards);
     for(let i in filter){
         if(filter[i].type === type){
             const card = document.createElement("div");
             card.classList.add(`card`)
             card.classList.add(filter[i][type]);
+            card.id = filter[i].id;
             const img = document.createElement('img');
             img.classList.add('item-img');
             img.src =`./assets/img/${filter[i].name.toLowerCase().replace(' ','-')}.jpeg`;
@@ -59,9 +106,13 @@ function render(filter,type){
             card.appendChild(harga);
             card.appendChild(status);
         }
-    }
-    
+    }   
 
 }
+
+function addItemToCart(items,name){
+    console.log(name);
+}
+
 
 
